9a82d2536ed4e36f716e1f2786843863b3d5dc8f
Howard Edidin <HEdidin@edidingroup.net>
2018-09-09 12:27:48 -0500

    ok

--- Quickstarts/3_ImplicitFlowAuthentication/src/QuickstartIdentityServer/Startup.cs
+++ Quickstarts/3_ImplicitFlowAuthentication/src/QuickstartIdentityServer/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace QuickstartIdentityServer
+namespace IdentityServer
 {
     public class Startup
     {
